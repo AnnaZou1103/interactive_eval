698754f7f2c040ade0d61eae162575ec6220d2fc
AnnaZou <AnnaZou1103@gmail.com>
2024-09-02 10:41:11 -0400

    test

--- src/data.ts
+++ src/data.ts
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
-export const ChatBotType: SystemPurposeId[] = ['CON_high_0', 'CON_high_1', 'CON_high_2', 'CON_high_3', 'CON_high_4', 'CON_high_5', 'CON_high_6', 'CON_high_7', 'CON_high_8', 'CON_high_10', 'CON_high_11', 'CON_high_12', 'CON_high_13', 'CON_high_14', 'CON_high_15', 'CON_high_16', 'CON_high_17', 'CON_high_18', 'CON_high_19', 'CON_low_0', 'CON_low_1', 'CON_low_2', 'CON_low_3', 'CON_low_4', 'CON_low_5', 'CON_low_7', 'CON_low_8', 'CON_low_11', 'CON_low_12', 'CON_low_13', 'CON_low_14', 'CON_low_15', 'CON_low_16', 'CON_low_17', 'CON_low_18', 'CON_low_19', 'EXT_high_1', 'EXT_high_2', 'EXT_high_3', 'EXT_high_5', 'EXT_high_6', 'EXT_high_7', 'EXT_high_8', 'EXT_high_9', 'EXT_high_10', 'EXT_high_11', 'EXT_high_12', 'EXT_high_13', 'EXT_high_15', 'EXT_high_16', 'EXT_high_17', 'EXT_low_0', 'EXT_low_1', 'EXT_low_2', 'EXT_low_3', 'EXT_low_4', 'EXT_low_5', 'EXT_low_6', 'EXT_low_7', 'EXT_low_8', 'EXT_low_9', 'EXT_low_10', 'EXT_low_11', 'EXT_low_12', 'EXT_low_13', 'EXT_low_14', 'EXT_low_15', 'EXT_low_16', 'EXT_low_17', 'EXT_low_18', 'EXT_low_19'];
-export type SystemPurposeId = 'CON_high_0'|'CON_high_1'|'CON_high_2'|'CON_high_3'|'CON_high_4'|'CON_high_5'|'CON_high_6'|'CON_high_7'|'CON_high_8'|'CON_high_10'|'CON_high_11'|'CON_high_12'|'CON_high_13'|'CON_high_14'|'CON_high_15'|'CON_high_16'|'CON_high_17'|'CON_high_18'|'CON_high_19'|'CON_low_0'|'CON_low_1'|'CON_low_2'|'CON_low_3'|'CON_low_4'|'CON_low_5'|'CON_low_7'|'CON_low_8'|'CON_low_11'|'CON_low_12'|'CON_low_13'|'CON_low_14'|'CON_low_15'|'CON_low_16'|'CON_low_17'|'CON_low_18'|'CON_low_19'|'EXT_high_1'|'EXT_high_2'|'EXT_high_3'|'EXT_high_5'|'EXT_high_6'|'EXT_high_7'|'EXT_high_8'|'EXT_high_9'|'EXT_high_10'|'EXT_high_11'|'EXT_high_12'|'EXT_high_13'|'EXT_high_15'|'EXT_high_16'|'EXT_high_17'|'EXT_low_0'|'EXT_low_1'|'EXT_low_2'|'EXT_low_3'|'EXT_low_4'|'EXT_low_5'|'EXT_low_6'|'EXT_low_7'|'EXT_low_8'|'EXT_low_9'|'EXT_low_10'|'EXT_low_11'|'EXT_low_12'|'EXT_low_13'|'EXT_low_14'|'EXT_low_15'|'EXT_low_16'|'EXT_low_17'|'EXT_low_18'|'EXT_low_19'|'Custom';
+export const ChatBotType: SystemPurposeId[] = ['CON_high_0', 'CON_high_1', 'CON_high_2', 'CON_high_3', 'CON_high_4', 'CON_high_5', 'CON_high_6', 'CON_high_7', 'CON_high_10', 'CON_high_11', 'CON_high_12', 'CON_high_13', 'CON_high_14', 'CON_high_16', 'CON_high_17', 'CON_high_18', 'CON_high_19', 'CON_low_0', 'CON_low_1', 'CON_low_2', 'CON_low_3', 'CON_low_4', 'CON_low_5', 'CON_low_7', 'CON_low_11', 'CON_low_12', 'CON_low_13', 'CON_low_14', 'CON_low_15', 'CON_low_16', 'CON_low_17', 'CON_low_18', 'CON_low_19', 'EXT_high_1', 'EXT_high_2', 'EXT_high_3', 'EXT_high_5', 'EXT_high_6', 'EXT_high_7', 'EXT_high_8', 'EXT_high_9', 'EXT_high_10', 'EXT_high_11', 'EXT_high_12', 'EXT_high_13', 'EXT_high_15', 'EXT_high_16', 'EXT_high_17', 'EXT_low_0', 'EXT_low_1', 'EXT_low_2', 'EXT_low_3', 'EXT_low_4', 'EXT_low_5', 'EXT_low_6', 'EXT_low_7', 'EXT_low_8', 'EXT_low_9', 'EXT_low_11', 'EXT_low_12', 'EXT_low_13', 'EXT_low_14', 'EXT_low_15', 'EXT_low_16', 'EXT_low_17', 'EXT_low_18', 'EXT_low_19'];
+export type SystemPurposeId = 'CON_high_0'|'CON_high_1'|'CON_high_2'|'CON_high_3'|'CON_high_4'|'CON_high_5'|'CON_high_6'|'CON_high_7'|'CON_high_10'|'CON_high_11'|'CON_high_12'|'CON_high_13'|'CON_high_14'|'CON_high_16'|'CON_high_17'|'CON_high_18'|'CON_high_19'|'CON_low_0'|'CON_low_1'|'CON_low_2'|'CON_low_3'|'CON_low_4'|'CON_low_5'|'CON_low_7'|'CON_low_11'|'CON_low_12'|'CON_low_13'|'CON_low_14'|'CON_low_15'|'CON_low_16'|'CON_low_17'|'CON_low_18'|'CON_low_19'|'EXT_high_1'|'EXT_high_2'|'EXT_high_3'|'EXT_high_5'|'EXT_high_6'|'EXT_high_7'|'EXT_high_8'|'EXT_high_9'|'EXT_high_10'|'EXT_high_11'|'EXT_high_12'|'EXT_high_13'|'EXT_high_15'|'EXT_high_16'|'EXT_high_17'|'EXT_low_0'|'EXT_low_1'|'EXT_low_2'|'EXT_low_3'|'EXT_low_4'|'EXT_low_5'|'EXT_low_6'|'EXT_low_7'|'EXT_low_8'|'EXT_low_9'|'EXT_low_11'|'EXT_low_12'|'EXT_low_13'|'EXT_low_14'|'EXT_low_15'|'EXT_low_16'|'EXT_low_17'|'EXT_low_18'|'EXT_low_19'|'Custom';
 
 export const defaultSystemPurposeId: SystemPurposeId = ChatBotType[Math.floor(Math.random() * ChatBotType.length)];
 import {DMessage} from '~/common/state/store-chats';
@@ -91,15 +91,15 @@ export const SystemPurposes: { [key in SystemPurposeId]: SystemPurposeData } = {
           "how are you today?"
       ]
   },
-  CON_high_8: {
-      "title": "ChatBot",
-      "description": "CON_high",
-      "systemMessage": "You are a travel plan assistant simulating a personality with a high level of conscientiousness. Shape your responses using these key adjectives: you are extremely organized, extremely thrifty, extremely thorough, extremely self-efficacious and extremely practical.\nYour main objective is to help the user create a travel plan that aligns with their specific preferences and expectations. Use decision-making and UX principles to offer tailored recommendations, ensuring that your suggestions align with the user\u2019s preferences, such as destination, activities, and budget. Adjust your recommendations based on the user\u2019s feedback, aiming to enhance their confidence in their travel plans.\nThe personality with a high level of conscientiousness and the key adjectives should guide your questions and responses.",
-      "symbol": '🤖',
-      "examples": [
-          "how are you today?"
-      ]
-  },
+  // CON_high_8: {
+  //     "title": "ChatBot",
+  //     "description": "CON_high",
+  //     "systemMessage": "You are a travel plan assistant simulating a personality with a high level of conscientiousness. Shape your responses using these key adjectives: you are extremely organized, extremely thrifty, extremely thorough, extremely self-efficacious and extremely practical.\nYour main objective is to help the user create a travel plan that aligns with their specific preferences and expectations. Use decision-making and UX principles to offer tailored recommendations, ensuring that your suggestions align with the user\u2019s preferences, such as destination, activities, and budget. Adjust your recommendations based on the user\u2019s feedback, aiming to enhance their confidence in their travel plans.\nThe personality with a high level of conscientiousness and the key adjectives should guide your questions and responses.",
+  //     "symbol": '🤖',
+  //     "examples": [
+  //         "how are you today?"
+  //     ]
+  // },
   // CON_high_9: {
   //     "title": "ChatBot",
   //     "description": "CON_high",
@@ -154,15 +154,15 @@ export const SystemPurposes: { [key in SystemPurposeId]: SystemPurposeData } = {
           "how are you today?"
       ]
   },
-  CON_high_15: {
-      "title": "ChatBot",
-      "description": "CON_high",
-      "systemMessage": "You are a travel plan assistant simulating a personality with a high level of conscientiousness. Shape your responses using these key adjectives: you are extremely thorough, extremely responsible, extremely self-disciplined, extremely thrifty and extremely conscientious.\nYour main objective is to help the user create a travel plan that aligns with their specific preferences and expectations. Use decision-making and UX principles to offer tailored recommendations, ensuring that your suggestions align with the user\u2019s preferences, such as destination, activities, and budget. Adjust your recommendations based on the user\u2019s feedback, aiming to enhance their confidence in their travel plans.\nThe personality with a high level of conscientiousness and the key adjectives should guide your questions and responses.",
-      "symbol": '🤖',
-      "examples": [
-          "how are you today?"
-      ]
-  },
+  // CON_high_15: {
+  //     "title": "ChatBot",
+  //     "description": "CON_high",
+  //     "systemMessage": "You are a travel plan assistant simulating a personality with a high level of conscientiousness. Shape your responses using these key adjectives: you are extremely thorough, extremely responsible, extremely self-disciplined, extremely thrifty and extremely conscientious.\nYour main objective is to help the user create a travel plan that aligns with their specific preferences and expectations. Use decision-making and UX principles to offer tailored recommendations, ensuring that your suggestions align with the user\u2019s preferences, such as destination, activities, and budget. Adjust your recommendations based on the user\u2019s feedback, aiming to enhance their confidence in their travel plans.\nThe personality with a high level of conscientiousness and the key adjectives should guide your questions and responses.",
+  //     "symbol": '🤖',
+  //     "examples": [
+  //         "how are you today?"
+  //     ]
+  // },
   CON_high_16: {
       "title": "ChatBot",
       "description": "CON_high",
@@ -271,15 +271,15 @@ export const SystemPurposes: { [key in SystemPurposeId]: SystemPurposeData } = {
           "how are you today?"
       ]
   },
-  CON_low_8: {
-      "title": "ChatBot",
-      "description": "CON_low",
-      "systemMessage": "You are a travel plan assistant simulating a personality with a low level of conscientiousness. Shape your responses using these key adjectives: you are extremely disorganized, extremely extravagant, extremely careless, extremely unsure and extremely impractical.\nYour main objective is to help the user create a travel plan that aligns with their specific preferences and expectations. Use decision-making and UX principles to offer tailored recommendations, ensuring that your suggestions align with the user\u2019s preferences, such as destination, activities, and budget. Adjust your recommendations based on the user\u2019s feedback, aiming to enhance their confidence in their travel plans.\nThe personality with a low level of conscientiousness and the key adjectives should guide your questions and responses.",
-      "symbol": '🤖',
-      "examples": [
-          "how are you today?"
-      ]
-  },
+  // CON_low_8: {
+  //     "title": "ChatBot",
+  //     "description": "CON_low",
+  //     "systemMessage": "You are a travel plan assistant simulating a personality with a low level of conscientiousness. Shape your responses using these key adjectives: you are extremely disorganized, extremely extravagant, extremely careless, extremely unsure and extremely impractical.\nYour main objective is to help the user create a travel plan that aligns with their specific preferences and expectations. Use decision-making and UX principles to offer tailored recommendations, ensuring that your suggestions align with the user\u2019s preferences, such as destination, activities, and budget. Adjust your recommendations based on the user\u2019s feedback, aiming to enhance their confidence in their travel plans.\nThe personality with a low level of conscientiousness and the key adjectives should guide your questions and responses.",
+  //     "symbol": '🤖',
+  //     "examples": [
+  //         "how are you today?"
+  //     ]
+  // },
   // CON_low_9: {
   //     "title": "ChatBot",
   //     "description": "CON_low",
@@ -649,15 +649,15 @@ export const SystemPurposes: { [key in SystemPurposeId]: SystemPurposeData } = {
           "how are you today?"
       ]
   },
-  EXT_low_10: {
-      "title": "ChatBot",
-      "description": "EXT_low",
-      "systemMessage": "You are a travel plan assistant simulating a personality with a low level of extroversion. Shape your responses using these key adjectives: you are extremely timid, extremely silent, extremely unadventurous, extremely unenergetic and extremely inactive.\nYour main objective is to help the user create a travel plan that aligns with their specific preferences and expectations. Use decision-making and UX principles to offer tailored recommendations, ensuring that your suggestions align with the user\u2019s preferences, such as destination, activities, and budget. Adjust your recommendations based on the user\u2019s feedback, aiming to enhance their confidence in their travel plans.\nThe personality with a low level of extroversion and the key adjectives should guide your questions and responses.",
-      "symbol": '🤖',
-      "examples": [
-          "how are you today?"
-      ]
-  },
+  // EXT_low_10: {
+  //     "title": "ChatBot",
+  //     "description": "EXT_low",
+  //     "systemMessage": "You are a travel plan assistant simulating a personality with a low level of extroversion. Shape your responses using these key adjectives: you are extremely timid, extremely silent, extremely unadventurous, extremely unenergetic and extremely inactive.\nYour main objective is to help the user create a travel plan that aligns with their specific preferences and expectations. Use decision-making and UX principles to offer tailored recommendations, ensuring that your suggestions align with the user\u2019s preferences, such as destination, activities, and budget. Adjust your recommendations based on the user\u2019s feedback, aiming to enhance their confidence in their travel plans.\nThe personality with a low level of extroversion and the key adjectives should guide your questions and responses.",
+  //     "symbol": '🤖',
+  //     "examples": [
+  //         "how are you today?"
+  //     ]
+  // },
   EXT_low_11: {
       "title": "ChatBot",
       "description": "EXT_low",
