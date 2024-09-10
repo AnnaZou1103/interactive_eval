ec91fe49ce2a3922e37ff9b5dfaf94c7cd53e790
AnnaZou <AnnaZou1103@gmail.com>
2024-09-09 20:13:27 -0400

    test

--- src/apps/news/AppNews.tsx
+++ src/apps/news/AppNews.tsx
@@ -57,7 +57,7 @@ export function AppNews() {
           <Typography level='h2' fontSize="xl"sx={{ mb: 0.5 }}  component='div'>Job Interview</Typography>
             Your task is to participate in a simulated job interview conducted by the chatbot.
           <ul style={{ marginTop: 8, marginBottom: 8, paddingInlineStart: 24 }}>
-            <li>Respond to the chatbot's questions as if you are a candidate in a real interview. The interview will focus on assessing your Organizational Citizenship Behaviors (OCB) such as initiative, helping, and compliance.</li>
+            <li>Respond to questions from the chatbot as if you are a candidate in a real interview. The interview will focus on assessing your Organizational Citizenship Behaviors (OCB) such as initiative, helping, and compliance.</li>
               <li>A possible <b>scenario</b> is : you are applying for the position of a purchasing guide.</li>
               <li>P.S. Please <b>don’t</b> disclose your private information during this conversational process. If you find it hard to start this task, ask the chatbot for an example.</li>
           </ul>
@@ -71,7 +71,7 @@ export function AppNews() {
           <Typography level='h2' fontSize="xl"sx={{ mb: 0.5 }}  component='div'>Public Services</Typography>
           The objective of this task is to use the chatbot to address work-related challenges.
           <ul style={{ marginTop: 8, marginBottom: 8, paddingInlineStart: 24 }}>
-            <li>Imagine you're a public service employee dealing with typical workday problems.</li>
+            <li>Imagine you are a public service employee dealing with typical workday problems.</li>
             <li>Describe the challenges you face, and the chatbot will guide you with suggestions.</li>
             <li>For more complex situations, tackle them step by step, and continue asking questions until all aspects are clear.</li>
           <li>A possible <b>scenario</b> is: you face resource allocation problem.</li>
